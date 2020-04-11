3aacef1792ea42ba453deb37ca76a8ebec8efb3f
Bharat Arya <arya.bharat@rocketmail.com>
2020-04-11 12:50:06 +0530

    v2 some changes like fire effect

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 let grid;
 let start = [0, 0];
 let end = [];
+let fire = [];
 let isMousePressed = false;
+let rowLimit;
+let colLimit;
 let moveStartEnd = {
     value: "",
     status: false
 }
 function createGrid() {
     let matrix = [];
-    let rowLimit = parseInt(document.getElementById("gridRow").value);
-    let colLimit = parseInt(document.getElementById("gridCol").value);
+    rowLimit = parseInt(document.getElementById("gridRow").value);
+    colLimit = parseInt(document.getElementById("gridCol").value);
     grid = new PF.Grid(rowLimit, colLimit);
     end = [rowLimit - 1, colLimit - 1];
+    start = [0,0]
     let tableHTML = "";
     for (let r = 0; r < rowLimit; r++) {
         let currentHTMLRow = `<tr id="row ${r}">`;
@@ -24,6 +28,8 @@ function createGrid() {
                 newNodeClass = "start";
             if (r == rowLimit - 1 && c == colLimit - 1)
                 newNodeClass = "end";
+            if (r == Math.floor(rowLimit / 2) && c == Math.floor(colLimit / 2))
+                newNodeClass = "fire";
             currentHTMLRow += `<td id="${newNodeId}" class="${newNodeClass}"></td>`;
         }
         tableHTML += `${currentHTMLRow}</tr>`;
@@ -41,9 +47,11 @@ function addEvents() {
             if (e.target.tagName === 'TD') {
                 if (e.target.className === 'wall') {
                     e.target.className = 'unvisited'
+                    let temp = (e.target.id).split('-');
+                    grid.setWalkableAt(temp[0], temp[1], true);
                 }
                 else {
-                    if (e.target.className == 'start' || e.target.className == 'end') {
+                    if (e.target.className == 'start' || e.target.className == 'end' || e.target.className == 'fire') {
                         moveStartEnd.status = true;
                         moveStartEnd.value = e.target.className;
                         e.target.className = 'unvisited'
@@ -58,12 +66,13 @@ function addEvents() {
         })
         cell.addEventListener('mouseenter', (e) => {
             if (e.target.tagName === 'TD' && isMousePressed == true) {
-                // console.log(e.target);
                 if (e.target.className === 'wall' && moveStartEnd.status == false) {
                     e.target.className = 'unvisited'
+                    let temp = (e.target.id).split('-');
+                    grid.setWalkableAt(temp[0], temp[1], true);
                 }
                 else {
-                    if (e.target.className == 'start' || e.target.className == 'end' ||  moveStartEnd.status == true) {
+                    if (e.target.className != 'wall' && moveStartEnd.status == true) {
                         e.target.className = (moveStartEnd.value || e.target.className);
                     }
                     else {
@@ -74,33 +83,61 @@ function addEvents() {
                 }
             }
         })
-        cell.addEventListener('mouseleave',(e)=>{
-            if (e.target.tagName === 'TD' && isMousePressed == true && moveStartEnd.status == true){            
-                    e.target.className = 'unvisited';
-
+        cell.addEventListener('mouseleave', (e) => {
+            if (e.target.tagName === 'TD' && e.target.className != 'wall' && isMousePressed == true && moveStartEnd.status == true) {
+                e.target.className = 'unvisited';
             }
         })
 
         cell.addEventListener('mouseup', (e) => {
-            if (moveStartEnd.status == true)
-              {
-                  e.target.className = moveStartEnd.value;
-                  if(moveStartEnd.value == 'start')
-                  start = (e.target.id).split('-');
-                  else
-                  end = (e.target.id).split('-');
-                  moveStartEnd.status = false;
-                  moveStartEnd.value = "";
-              }
-                    isMousePressed = false;
+            if (moveStartEnd.status == true) {
+                e.target.className = moveStartEnd.value;
+                if (moveStartEnd.value == 'start')
+                    start = (e.target.id).split('-');
+                else if (moveStartEnd.value == 'end')
+                    end = (e.target.id).split('-');
+                else
+                    fire = (e.target.id).split('-').map(Number);
+                moveStartEnd.status = false;
+                moveStartEnd.value = "";
+            }
+            isMousePressed = false;
         })
     })
 }
 
 function findPath() {
     var finder = new PF.AStarFinder();
-    // console.log(start[0], start[1], end[0], end[1]);
     var path = finder.findPath(start[0], start[1], end[0], end[1], grid);
+    // console.log(path.length)
+    let personSpeed = parseInt(document.getElementById("personSpeed").value);
+    let fireRate = parseInt(document.getElementById("fireRate").value);
+    let time = Math.ceil(path.length / personSpeed)
+    let blocks = Math.ceil(time / fireRate);
+    // console.log(blocks);
+    fire[0] -= blocks;
+    fire[1] -= blocks;
+    for (let i = 0; i < (blocks * 2); i++) {
+        for (let j = 0; j < (blocks * 2); j++) {
+            console.log( (fire[0]+i) + '-' + (fire[1]+j))
+            if((fire[0]+i) < rowLimit && (fire[1]+j) < colLimit && (fire[0]+i) >= 0 && (fire[1]+j) >= 0){
+                console.log( (fire[0]+i) + '-' + (fire[1]+j))
+                document.getElementById( (fire[0]+i) + '-' + (fire[1]+j)).className = 'fire';
+            }
+        }
+    }
+    // for(let i=0;i<blocks;i++)
+    // {
+    //     let temp  = [5,5];
+    //     document.getElementById( (temp[0]+i) + '-' + (temp[1]+i)).className = 'fire';
+    //     document.getElementById( (temp[0]+i) + '-' + (temp[1])).className = 'fire';
+    //     document.getElementById( (temp[0]+i) + '-' + (temp[1]-i)).className = 'fire';
+    //     document.getElementById( (temp[0]) + '-' + (temp[1]+i)).className = 'fire';
+    //     document.getElementById( (temp[0]-i) + '-' + (temp[1]+i)).className = 'fire';
+    //     document.getElementById( (temp[0]-i) + '-' + (temp[1]+i)).className = 'fire';
+    //     document.getElementById( (temp[0]) + '-' + (temp[1]-i)).className = 'fire';
+    //     document.getElementById( (temp[0]-i) + '-' + (temp[1]+i)).className = 'fire'
+    // }
     path.forEach(i => {
         document.getElementById(i[0] + '-' + i[1]).className = 'visited'
     });
